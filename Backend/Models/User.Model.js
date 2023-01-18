63fae8fd3c30579c33f637da881d20c17e0426db
Avinash Patel <avinashpatel9755@gmail.com>
2023-01-18 14:35:49 +0530

    Adding

---
+++ Backend/Models/User.Model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    age: { type: Number, required: true },
+    gender: { type: String, required: true },
+    address: { type: String, required: true },
+    pincode: { type: Number, required: true },
+    phone: { type: Number, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isUser: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: false },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = {
+  UserModel,
+};
