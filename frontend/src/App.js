8610d87e36ccf3ea4ab7e6ada0ac0cf087894a4f
Vaibhav <vaibhav.2071998@gmail.com>
2023-01-19 22:22:23 +0530

    added day1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Poster from "./components/Poster";
+import Home from "./pages/routes/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
       <Poster />
       <Footer />
     </div>
