3917a4cd3d5e8a0dbbd6e0c0a3c0617ad781ee4d
Vaibhav <vaibhav.2071998@gmail.com>
2023-01-21 00:13:15 +0530

    added day3 work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Cart from "./components/Cart";
 import Admin from "./pages/routes/Admin";
-
+import AllRoutes from "./pages/routes/AllRoutes";
+import Home from "./pages/routes/Home";
+import Navbar from "./components/HomePage/Navbar";
+import Footer from "./components/HomePage/Footer";
 
 const App = () => {
   return (
     <div className="App">
       {/* <Cart /> */}
-      <Admin />
+      {/* <Admin /> */}
+      <Navbar />
+      <AllRoutes />
+      <Footer />
     </div>
   );
 };
